b90fdaa5de2d65518600331547ae3c334cafe3f2
Mojtaba Khatibi <Khatibi486@gmail.com>
2025-09-16 16:31:48 +0330

    add suspense tag to products file

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -4,7 +4,7 @@ import getAllPosts from "@/modules/getAllPosts";
 import PostCard from "@/modules/postCard";
 import { useQuery } from "@tanstack/react-query";
 import styles from "./products.module.css";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Spinner from "@/modules/loader/Loader";
 function Products() {
@@ -39,13 +39,13 @@ function Products() {
       setShowData(filtered);
       setshowFilteredData(true);
     } else if (search && !isPending) {
-      console.log(search)
+      console.log(search);
       const searched = data.filter((item) =>
         item.name.trim().toLowerCase().includes(search.trim().toLowerCase())
       );
       setShowData(searched);
       setshowFilteredData(true);
-    }else if(category && search && !isPending){
+    } else if (category && search && !isPending) {
       const filtered = data.filter((item) => item.category === category);
       const searchedOnFilter = filtered.filter((item) =>
         item.name.trim().toLowerCase().includes(search.trim().toLowerCase())
@@ -64,47 +64,49 @@ function Products() {
   if (isPending) return <Spinner />;
 
   return (
-    <div className={styles.container}>
-      <div>
-        {showFilteredData ? (
-          <button
-            onClick={() => (
-              setshowFilteredData(false), router.push("/products")
-            )}
-          >
-            حذف  فیلتر
-          </button>
-        ) : null}
-        <h4>دسته بندی : </h4>
-        <ul>
-          {!isPending &&
-            categories.map((item, index) => (
-              <li key={index} onClick={() => categoryHandler(item)}>
-                {item}
-              </li>
-            ))}
-        </ul>
+    <Suspense fallback={<Spinner />}>
+      <div className={styles.container}>
+        <div>
+          {showFilteredData ? (
+            <button
+              onClick={() => (
+                setshowFilteredData(false), router.push("/products")
+              )}
+            >
+              حذف فیلتر
+            </button>
+          ) : null}
+          <h4>دسته بندی : </h4>
+          <ul>
+            {!isPending &&
+              categories.map((item, index) => (
+                <li key={index} onClick={() => categoryHandler(item)}>
+                  {item}
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className={styles.allCards}>
+          {!showFilteredData ? (
+            <>
+              {data.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </>
+          ) : (
+            <>
+              {showData.length > 0 ? (
+                showData.map((post) => <PostCard key={post._id} post={post} />)
+              ) : (
+                <h3 className={styles.noProduct}>
+                  محصولی در این دسته بندی وجود ندارد
+                </h3>
+              )}
+            </>
+          )}
+        </div>
       </div>
-      <div className={styles.allCards}>
-        {!showFilteredData ? (
-          <>
-            {data.map((post) => (
-              <PostCard key={post._id} post={post} />
-            ))}
-          </>
-        ) : (
-          <>
-            {showData.length > 0 ? (
-              showData.map((post) => <PostCard key={post._id} post={post} />)
-            ) : (
-              <h3 className={styles.noProduct}>
-                محصولی در این دسته بندی وجود ندارد
-              </h3>
-            )}
-          </>
-        )}
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
